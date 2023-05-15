284ca9a204503e0cd8d4d322bb31c1b5009bcb91
Meelan-98 <meelanbandara@gmail.com>
2023-05-15 20:47:48 +0530

    delete added

--- converters/build_rule.py
+++ converters/build_rule.py
@@ -8,7 +8,6 @@ def is_approved(params):
 
     return(commands)
 
-
 def write_listing(params):
             
     commands = [str("s_listings["+params[0]+"]["+params[1]+"] = Listing_sell(msg.sender," + params[0] + "," + params[1] + "," + params[2] + ");")]
@@ -24,11 +23,22 @@ def update_listing(params):
 
     return(commands)
 
+def delete_listing(params):
+            
+    commands = [str("delete s_listings["+params[0]+"]["+params[1]+"];")]
+    commands.append(str("EnumerableSet.remove(s_address_tokens[" + params[0] + "]," + params[1] + ");"))
+    commands.append(str("if (EnumerableSet.length(s_address_tokens[" + params[0] + "]) == 0) {"))
+    commands.append(str("    EnumerableSet.remove(s_address," + params[0] + ");"))
+    commands.append(str("}"))
+    commands.append("s_listed.decrement();\n")
+
+    return(commands)
+
 function_map = {
     "is_approved" : is_approved,
     "write_listing" : write_listing,
     "update_listing" : update_listing,
-    "delete_listing" : "function3",
+    "delete_listing" : delete_listing,
     "load_listing" : "function4",
     "is_price_sufficient" : "function5",
     "add_proceeds" : "function6",
