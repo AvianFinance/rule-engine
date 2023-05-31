e0589b0d856e4a6eaec44ef04371f79ec334fdba
Meelan-98 <meelanbandara@gmail.com>
2023-05-31 18:10:03 +0530

    Update comparer.py

--- comparer.py
+++ comparer.py
@@ -1,22 +1,3 @@
-# def compare_files(file1, file2):
-#     with open(file1, 'r') as f1, open(file2, 'r') as f2:
-#         lines1 = f1.readlines()
-#         lines2 = f2.readlines()
-
-#         total_lines = min(len(lines1), len(lines2))
-#         matched_lines = sum(1 for line1, line2 in zip(lines1, lines2) if line1.strip() == line2.strip() and line1.strip())
-
-#         score = matched_lines / total_lines
-#         return score
-
-
-# # Example usage
-file1_path = 'contracts/main/sell_logic.txt'  # Path to the first text file
-file2_path = 'contracts/new/sell_logic.txt'  # Path to the second text file
-
-# score = compare_files(file1_path, file2_path)
-# print(f"Matching score: {score}")
-
 def compare_files(file1_path, file2_path):
     with open(file1_path, 'r') as file1, open(file2_path, 'r') as file2:
         for line_num, (line1, line2) in enumerate(zip(file1, file2), start=1):
@@ -25,9 +6,9 @@ def compare_files(file1_path, file2_path):
     
     return "Looks great! Files are similar."
 
-# Example usage
-# file1_path = 'file1.txt'
-# file2_path = 'file2.txt'
+file1_path = 'contracts/main/sell_logic.txt'
+file2_path = 'contracts/new/sell_logic.txt'
+
 result = compare_files(file1_path, file2_path)
 print(result)
 
