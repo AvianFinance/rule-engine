5fbdd61f6893ad80f2c60b66422af8ba30cbcd8e
Meelan-98 <meelanbandara@gmail.com>
2023-06-01 13:58:38 +0530

    fetch rules added

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from sell_compile import compile
+from fetch_rules import fetch_rules
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,3 +12,7 @@ def index():
 def say_hello():
   compile()
   return 'Hello from Server'
+
+@app.route('/fetch/<contract_type>/<rule_type>')
+def get_contract_level_rules(contract_type,rule_type):
+  return jsonify(data=fetch_rules(contract_type,rule_type))
