ebe4a367eb10982f963d2614a1f09ce9ba7b5801
Meelan-98 <meelanbandara@gmail.com>
2023-06-02 11:46:40 +0530

    automatic compiling fixed

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from services.upload_contract import upload_contract
 from fetch_rules import fetch_rules
 import asyncio
 from flask_cors import CORS
+from sm_handler.deploy import compile_export_contract
 
 app = Flask(__name__)
 
@@ -12,12 +13,15 @@ CORS(app)
 async def async_upload_contract(contract_name):
     return upload_contract(contract_name)
 
+async def async_deploy_contract(contract_name):
+    return compile_export_contract(contract_name)
+
 @app.route('/')
 def index():
   return 'Server Works!'
   
-@app.route('/compile/<contract_type>', methods = ['POST'])
-def compile_upload_contract(contract_type):
+@app.route('/check/<contract_type>', methods = ['POST'])
+def write_upload_contract(contract_type):
   compile_status = contract_compile(contract_type)
   if (compile_status=="Compiling Successful"):
     loop = asyncio.new_event_loop()
@@ -28,6 +32,16 @@ def compile_upload_contract(contract_type):
     return jsonify('https://gateway.pinata.cloud/ipfs/' + str(result))
   else:
     return("compiling failed")
+  
+@app.route('/deploy/<contract_type>')
+def compile_deploy_contract(contract_type):
+
+  loop = asyncio.new_event_loop()
+  asyncio.set_event_loop(loop)
+  result = loop.run_until_complete(async_deploy_contract(contract_type.capitalize()))
+  loop.close()
+  print("result--------------"+ str(result))
+  return str(result)
     
 
 @app.route('/fetch/<contract_type>/<rule_type>')
