52b9e68a3a58e21335005add48dbec5efb92f4bb
Isuru Ariyarathne <61895498+IsuruAriyarathne@users.noreply.github.com>
2023-06-08 15:22:04 +0530

    add latest contract endpoint

--- services/db.py
+++ services/db.py
@@ -25,10 +25,8 @@ def insert_collection(new_contract):
         client = MongoClient("mongodb+srv://avfx_root:irmiot4462281@avianfinance.qc7bqtj.mongodb.net/?retryWrites=true&w=majority")
         db = client["AVFX"]
         collection = db["upgraded-contracts"]
-        print(new_contract)
         collection.update_many({ "status": { "$in": ["pending", "deployed"] } }, {"$set": {"status": "overwritten"}})
         collection.insert_one(new_contract)
-        print("inserted")
     except Exception as e:
         return(e)
     
@@ -37,13 +35,25 @@ def get_collections():
         client = MongoClient("mongodb+srv://avfx_root:irmiot4462281@avianfinance.qc7bqtj.mongodb.net/?retryWrites=true&w=majority")
         db = client["AVFX"]
         collection = db["upgraded-contracts"]
-        print('--------------------')
         upgraded_contracts = collection.find({}, {"_id": 0})
         upgraded_smartcontracts_list = []
         for contract in upgraded_contracts:
-            print(contract)
             upgraded_smartcontracts_list.append(contract)
         return (["Success",upgraded_smartcontracts_list])
+    except Exception as e:
+        return(["Error",e])
+    
+def getlattest(contract_type):
+    try:
+        client = MongoClient("mongodb+srv://avfx_root:irmiot4462281@avianfinance.qc7bqtj.mongodb.net/?retryWrites=true&w=majority")
+        db = client["AVFX"]
+        collection = db["upgraded-contracts"]
+        lattestcontract = collection.find({"contract_name": contract_type,"status": "upgraded"},{"_id": 0} ).limit(1);
+        upgradedcontracts = []
+        for contract in lattestcontract:
+            print(contract)
+            upgradedcontracts.append(contract)
+        return (["Success",upgradedcontracts])
     except Exception as e:
         return(["Error",e])
     
