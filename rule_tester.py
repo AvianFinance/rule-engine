beafc5ebccb08e0a5f30e355d95274da230ff30f
Meelan-98 <meelanbandara@gmail.com>
2023-06-16 00:42:34 +0530

    sell handled terminalogy

--- rule_tester.py
+++ rule_tester.py
@@ -1,5 +1,5 @@
 from sm_handler.writer import write_contract
 from services.comparer import compare_contract
 
-write_contract("ins","check")
-compare_contract("ins")
+write_contract("sell","check")
+compare_contract("sell")
