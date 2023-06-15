06551c1ba9b0d7fab0c539495c55798f768dcac3
Meelan-98 <meelanbandara@gmail.com>
2023-06-15 11:36:08 +0530

    sell imports issue fixed

--- rule_tester.py
+++ rule_tester.py
@@ -1,5 +1,5 @@
 from sm_handler.writer import write_contract
 from services.comparer import compare_contract
 
-write_contract("rent","check")
-compare_contract("rent")
+write_contract("sell","check")
+compare_contract("sell")
